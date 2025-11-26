165817eb557656f5f8e89ed7f4f6954f7d5ea090
Admin <alinakhamzina.6@gmail.com>
2025-11-26 23:40:58 +0300

    добавили ф-ю проверки н ф-и поиска двоичных чисел в тексте

--- tests.py
+++ tests.py
@@ -23,3 +23,8 @@ class TestBinaryUtils(unittest.TestCase):
         self.assertFalse(is_binary_string("10201"))
         self.assertFalse(is_binary_string("abc"))
         self.assertFalse(is_binary_string("10 10"))
+
+    def test_find_binaries_in_text_basic(self):
+        text = "values: 1010, 102, 000111, hello, 1110001."
+        result = find_binaries_in_text(text)
+        self.assertEqual(result, ["1010", "000111", "1110001"])
