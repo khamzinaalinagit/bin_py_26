759c9a8a10ea7bd946db62145d86819607a645dd
Admin <alinakhamzina.6@gmail.com>
2025-11-26 23:36:31 +0300

    добавили ф-ю которая скачивает веб-страницу по URL и ищет в ней все двоичные числа.

--- funcs.py
+++ funcs.py
@@ -18,3 +18,10 @@ def find_binaries_in_file(path, encoding: str = "utf-8"):
     with open(path, "r", encoding=encoding) as f:
         content = f.read()
     return find_binaries_in_text(content)
+
+
+def find_binaries_in_url(url, timeout):
+    response = requests.get(url, timeout=timeout)
+    response.raise_for_status()
+    text = response.text
+    return find_binaries_in_text(text)
