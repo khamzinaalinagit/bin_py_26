50da363f7b4f2b9d9dd64472fdee5b10e1d16e71
Admin <alinakhamzina.6@gmail.com>
2025-11-26 23:40:08 +0300

    добавили ф-ю проверки ф-и поиска двоичных чисел в строке

--- tests.py
+++ tests.py
@@ -8,3 +8,11 @@ from funcs import (
     find_binaries_in_file,
 )
 
+
+class TestBinaryUtils(unittest.TestCase):
+    def test_is_binary_string_valid(self):
+        self.assertTrue(is_binary_string("0"))
+        self.assertTrue(is_binary_string("1"))
+        self.assertTrue(is_binary_string("1010"))
+        self.assertTrue(is_binary_string("000111"))
+        self.assertTrue(is_binary_string("   1011   "))
