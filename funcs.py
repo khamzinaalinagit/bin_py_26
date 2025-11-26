9f09c49f59c8862ba8c6c35f973affacc470b318
Admin <alinakhamzina.6@gmail.com>
2025-11-26 23:34:02 +0300

    добавление функции проверки, что строка s является двоичным числом

--- funcs.py
+++ funcs.py
@@ -3,3 +3,8 @@ import requests
 
 BINARY_IN_TEXT_PATTERN = re.compile(r'\b[01]+\b')
 BINARY_FULL_PATTERN = re.compile(r'^[01]+$')
+
+
+def is_binary_string(s):
+    s = s.strip()
+    return bool(BINARY_FULL_PATTERN.fullmatch(s))
